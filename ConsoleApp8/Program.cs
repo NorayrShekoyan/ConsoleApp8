fdc3eea31d9a83ff0b5b9167c87b69ad4744d4ef
Admin110 <Admin110@ADMIN>
2023-06-04 18:19:20 +0400

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (double i = 50; i <= 70; i++)
+            {
+                while (i != 1)
+                {
+                    if (i % 2 == 0)
+                    {
+                        i /= 2;
+                    }
+                    else
+                    {
+                        i = (i * 3 + 1) / 2;
+                    }
+                }
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
